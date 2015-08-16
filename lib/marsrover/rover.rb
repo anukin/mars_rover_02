a872bd30f8cd15f72cb5b10a5340d176a8f85d27
anukin <anukin2611@gmail.com>
2015-08-16 15:54:25 +0530

    direction specs are passing

--- lib/marsrover/rover.rb
+++ lib/marsrover/rover.rb
@@ -1,7 +1,12 @@
 module MarsRoverApp
-  R = "right"
+
   E = "east"
   N = "north"
+  S = "south"
+  W = "west"
+
+  CLOCKWISE_DIRECTION = [N, E, S, W]
+  ANTI_CLOCKWISE_DIRECTION = [N, W, S, E]
   class Rover
     #rover models a mars rover with position direction and commands to be obeyed
     attr_reader :direction
@@ -12,7 +17,14 @@ module MarsRoverApp
     end
 
     def command(command)
-      @direction = N
+      case command
+      when "R"
+        clockwise_rotation = (CLOCKWISE_DIRECTION.index(@direction) + 1) % 4
+        @direction = CLOCKWISE_DIRECTION[clockwise_rotation]
+      when "L"
+        anti_clockwise_rotation = (ANTI_CLOCKWISE_DIRECTION.index(@direction) + 1) % 4
+        @direction = ANTI_CLOCKWISE_DIRECTION[anti_clockwise_rotation]
+      end
     end
   end
 end
--- spec/marsrover/rover_spec.rb
+++ spec/marsrover/rover_spec.rb
@@ -4,7 +4,13 @@ module MarsRoverApp
     context "movement" do
       it "should change direction clockwise if command is R" do
         rover = Rover.new(0, 0, E)
-        rover.command(R)
+        rover.command("R")
+        expect(rover.direction).to eq(S)
+      end
+
+      it "should change its direction anticlockwise if command is L" do
+        rover = Rover.new(0, 0, E)
+        rover.command("L")
         expect(rover.direction).to eq(N)
       end
     end
