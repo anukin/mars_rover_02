d60003d571f5554cca61647672dc611e2cdddc33
anukin <anukin2611@gmail.com>
2015-08-16 15:54:48 +0530

    Movement along x-axis implemented

--- lib/marsrover/rover.rb
+++ lib/marsrover/rover.rb
@@ -9,7 +9,7 @@ module MarsRoverApp
   ANTI_CLOCKWISE_DIRECTION = [N, W, S, E]
   class Rover
     #rover models a mars rover with position direction and commands to be obeyed
-    attr_reader :direction
+    attr_reader :direction, :x_cordinate, :y_cordinate
     def initialize(x_cordinate, y_cordinate, direction)
       @x_cordinate = x_cordinate
       @y_cordinate = y_cordinate
@@ -21,9 +21,13 @@ module MarsRoverApp
       when "R"
         clockwise_rotation = (CLOCKWISE_DIRECTION.index(@direction) + 1) % 4
         @direction = CLOCKWISE_DIRECTION[clockwise_rotation]
+      
       when "L"
         anti_clockwise_rotation = (ANTI_CLOCKWISE_DIRECTION.index(@direction) + 1) % 4
         @direction = ANTI_CLOCKWISE_DIRECTION[anti_clockwise_rotation]
+      
+      when "M"
+        @x_cordinate = 1
       end
     end
   end
--- spec/marsrover/rover_spec.rb
+++ spec/marsrover/rover_spec.rb
@@ -2,16 +2,20 @@ require 'spec_helper'
 module MarsRoverApp
   describe "rover" do
     context "movement" do
+      let(:rover) { Rover.new(0, 0, E) }
       it "should change direction clockwise if command is R" do
-        rover = Rover.new(0, 0, E)
         rover.command("R")
         expect(rover.direction).to eq(S)
       end
 
       it "should change its direction anticlockwise if command is L" do
-        rover = Rover.new(0, 0, E)
         rover.command("L")
         expect(rover.direction).to eq(N)
+      end
+
+      it "should move forward along x direction when given M" do
+        rover.command("M")
+        expect(rover.x_cordinate).to eq(1)
       end
     end
   end
